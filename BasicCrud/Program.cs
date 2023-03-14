d9dd20952798f2e89d21068108eaeee5db9913a9
Just Gavin <gavinokeeffe01@gmail.com>
2023-03-14 12:25:24 +0000

    Create, Read and Update working as intended

--- BasicCrud/Program.cs
+++ BasicCrud/Program.cs
@@ -17,6 +17,7 @@ namespace SimpleCrudSystem
                 Console.WriteLine("1. Add user");
                 Console.WriteLine("2. List user by email");
                 Console.WriteLine("3. List all users");
+                Console.WriteLine("4. Update a User");
 
                 // Get user input
                 string input = Console.ReadLine();
@@ -34,6 +35,10 @@ namespace SimpleCrudSystem
                     case "3":
                         // Option to list all users
                         ListAllUsers();
+                        break;
+                    case "4":
+                        // Option to Update a user
+                        UpdateUser();
                         break;
                     default:
                         Console.WriteLine("Invalid input");
@@ -81,13 +86,54 @@ namespace SimpleCrudSystem
             }
         }
 
-        static void ListAllUsers()
+
+
+        static void ListAllUsers() //list all users
         {
             Console.WriteLine("List of all users:");
 
             foreach (User user in users)
             {
                 Console.WriteLine($"Name: {user.Name}, Email: {user.Email}");
+            }
+        }
+
+        static void UpdateUser()
+        {
+            // List all the users
+            ListAllUsers();
+
+            // Get user's email address to update
+            Console.Write("Enter email address of user to update: ");
+            string email = Console.ReadLine();
+
+            // Find user with matching email address
+            User userToUpdate = users.Find(u => u.Email == email);
+
+            // If user is found, prompt for new name and email address and update user
+            if (userToUpdate != null)
+            {
+                Console.Write("Enter new name (press Enter to keep existing name): ");
+                string name = Console.ReadLine();
+
+                Console.Write("Enter new email address (press Enter to keep existing email address): ");
+                string newEmail = Console.ReadLine();
+
+                if (!string.IsNullOrWhiteSpace(name))
+                {
+                    userToUpdate.Name = name;
+                }
+
+                if (!string.IsNullOrWhiteSpace(newEmail))
+                {
+                    userToUpdate.Email = newEmail;
+                }
+
+                Console.WriteLine("User updated successfully");
+            }
+            else
+            {
+                Console.WriteLine("User not found");
             }
         }
     }
