26f131329626afe4b50bdc2c59a1cfe064ad5fff
Just Gavin <gavinokeeffe01@gmail.com>
2023-03-14 12:04:15 +0000

    Create User Implemented

--- BasicCrud/Program.cs
+++ BasicCrud/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleCrudSystem
+{
+    class Program
+    {
+        static List<User> users = new List<User>(); // A list called User to store user data
+
+        static void Main(string[] args)
+        {
+            // This gets the user input for name and email
+            Console.Write("Enter user name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter user email: ");
+            string email = Console.ReadLine();
+
+            // Adds email and name to the user 
+            User newUser = new User(name, email);
+
+            // Adds user to the list
+            users.Add(newUser);
+
+            Console.WriteLine("User created successfully");
+
+            static void ReadUser()
+            {
+                // Get user input
+                Console.Write("Enter user email: ");
+                string email = Console.ReadLine();
+
+                // Find user in the list by email
+                User user = users.Find(u => u.Email == email);
+
+                if (user != null)
+                {
+                    // Display user information
+                    Console.WriteLine("Name: {0}", user.Name);
+                    Console.WriteLine("Email: {0}", user.Email);
+                }
+                else
+                {
+                    Console.WriteLine("User not found");
+                }
+            }
+        }
+
+        
+    }
+
+    class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public User(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+    }
+}
